21964cd5a78f2d7954773a2bf76a8b5ec2e6a2d2
Michael Burton <mjburton11@Michaels-MacBook-Pro.local>
2021-05-11 11:24:38 -0400

    add async pipe and simplify

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -9,22 +9,20 @@ import { Supplier } from '../suppliers/supplier';
 import { SupplierService } from '../suppliers/supplier.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   private productsUrl = 'api/products';
   private suppliersUrl = this.supplierService.suppliersUrl;
 
-  constructor(private http: HttpClient,
-              private supplierService: SupplierService) { }
-
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(data => console.log('Products: ', JSON.stringify(data))),
-        catchError(this.handleError)
-      );
-  }
+  products$ = this.http.get<Product[]>(this.productsUrl).pipe(
+    tap((data) => console.log('Products: ', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
+  constructor(
+    private http: HttpClient,
+    private supplierService: SupplierService
+  ) {}
 
   private fakeProduct(): Product {
     return {
@@ -35,7 +33,7 @@ export class ProductService {
       price: 8.9,
       categoryId: 3,
       // category: 'Toolbox',
-      quantityInStock: 30
+      quantityInStock: 30,
     };
   }
 
@@ -54,5 +52,4 @@ export class ProductService {
     console.error(err);
     return throwError(errorMessage);
   }
-
 }
