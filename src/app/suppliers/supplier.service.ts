934bb55866d92e98dc19564d1a7e6f637970cf1c
Michael Burton <mjburton11@Michaels-MacBook-Pro.local>
2021-05-19 11:21:28 -0400

    add high order observables

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -1,15 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError, Observable } from 'rxjs';
+import { throwError, Observable, of } from 'rxjs';
+import { Supplier } from './supplier';
+import { concatMap, map, mergeMap, switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  suppliersWithMap$ = of(1, 5, 8).pipe(
+    map((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+  suppliersWithConcatMap$ = of(1, 5, 8).pipe(
+    tap((id) => console.log('concatMap source Observable', id)),
+    concatMap((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+  suppliersWithMergeMap$ = of(1, 5, 8).pipe(
+    tap((id) => console.log('mergeMap source Observable', id)),
+    mergeMap((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+  private _suppliersWithSwitchMap$ = of(1, 5, 8).pipe(
+    tap((id) => console.log('switchMap source Observable', id)),
+    switchMap((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+  public get suppliersWithSwitchMap$() {
+    return this._suppliersWithSwitchMap$;
+  }
+  public set suppliersWithSwitchMap$(value) {
+    this._suppliersWithSwitchMap$ = value;
+  }
+
+  constructor(private http: HttpClient) {
+    this.suppliersWithMap$.subscribe((o) =>
+      o.subscribe((item) => console.log('map result', item))
+    );
+    this.suppliersWithConcatMap$.subscribe((item) =>
+      console.log('concatMap result', item)
+    );
+    this.suppliersWithMergeMap$.subscribe((item) =>
+      console.log('mergeMap result', item)
+    );
+    this.suppliersWithSwitchMap$.subscribe((item) =>
+      console.log('switchMap result', item)
+    );
+  }
 
   private handleError(err: any): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
@@ -26,5 +64,4 @@ export class SupplierService {
     console.error(err);
     return throwError(errorMessage);
   }
-
 }
